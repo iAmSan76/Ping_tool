f4d6c9273d128d6f91ad2cea76e66ce88c8e3eb8
iAmSan76 <118990488+iAmSan76@users.noreply.github.com>
2023-06-30 14:40:29 -0500

    Add files via upload

---
+++ exaple_nslookup.py
@@ -0,0 +1,19 @@
+import subprocess
+
+direccion_ip = "8.8.8.8"  # Dirección IP de Google
+
+comando_ping = ["ping", "-n", "4", direccion_ip]
+
+resultado = subprocess.Popen(comando_ping, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+salida_stdout, salida_stderr = resultado.communicate()
+
+# Decodificar la salida en bytes a formato de texto
+salida_stdout = salida_stdout.decode()
+
+# Buscar la línea que indica el porcentaje de pérdida de paquetes
+lineas = salida_stdout.split("\n")
+for linea in lineas:
+    if "packet loss" in linea:
+        porcentaje_perdida = linea.split(",")[-1].strip()
+        print("Porcentaje de pérdida de paquetes:", porcentaje_perdida)
+        break
